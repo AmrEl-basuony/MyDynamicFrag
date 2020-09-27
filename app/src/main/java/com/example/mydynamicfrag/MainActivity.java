51fa1cd6217805a54d71e7086096f5f865a90c3e
Amr El-basuony <45123130+Hiden-Evil@users.noreply.github.com>
2020-09-27 09:51:50 +0200

    Initial commit

---
+++ app/src/main/java/com/example/mydynamicfrag/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.mydynamicfrag;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        MyFragment myFragment=new MyFragment();
+        FragmentManager mgr = getFragmentManager();
+        FragmentTransaction fragTran=mgr.beginTransaction();
+        fragTran.add(R.id.activity_main_layout,myFragment,"myfragment");
+        fragTran.commit();
+    }
+}
---
+++ app/src/main/java/com/example/mydynamicfrag/MyFragment.java
@@ -0,0 +1,17 @@
+package com.example.mydynamicfrag;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class MyFragment extends Fragment {
+    @Nullable
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_appearance,container,false);
+    }
+}
